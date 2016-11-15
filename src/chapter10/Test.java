5d41d49dff359f6cd612c16e31528ee807bb9229
wangbiwen [王必文] <wangbiwen@xiaomi.com>
2016-11-15 21:29:06 +0800

    add a test code.

---
+++ src/chapter10/Test.java
@@ -0,0 +1,13 @@
+package chapter10;
+
+/**
+ * FileName: Test.java
+ * Description:
+ * Authors: wangbiwen<wangbiwen@xiaomi.com>
+ * Date: 16-11-15
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
